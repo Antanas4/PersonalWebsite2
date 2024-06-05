770fc98e9b702ce448081618ee14809abb6d67d7
Antanas4 <antanas.buteliauskas@gmail.com>
2024-06-05 10:33:16 +0300

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
+// import backgroundImage from "./../assets/photo/photos/Untitled-1-01.jpg";
+// import backgroundImage from "./../assets/photo/photos/Untitled-1-02.jpg";
+import backgroundImage from "./../assets/photo/photos/Untitled-1-03.jpg";
 
 export const SectionHome = () => {
-    return (
-        <section id="home">
-            <div className="header-content">
-                <h1>Hi, my name is</h1>
-                <h2>Antanas Buteliauskas</h2>
-                <h3>I'm a junior software developer</h3>
-            </div>
-        </section>
-    );
+  const sectionStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "100vh",
+  };
+
+  return (
+    <div className="home" style={sectionStyle}>
+      <div className="header-content">
+        <h1>Hi, my name is</h1>
+        <h2>Antanas Buteliauskas</h2>
+        <h3>I'm a junior software developer</h3>
+      </div>
+    </div>
+  );
 };
