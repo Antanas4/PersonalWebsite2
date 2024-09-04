f0a65863005cfcbb463a85004eaead7392da22da
Antanas4 <antanas.buteliauskas@gmail.com>
2024-09-05 01:41:11 +0300

    redesigning in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ function Header(props) {
   return (
     <div className={props.className}>
       <h1>
-        {/* <span className="header-star">*</span> */}
         <span className={props.headerText}>{props.headerTitle}</span>
       </h1>
     </div>
